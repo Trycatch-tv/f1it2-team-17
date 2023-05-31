e8050a3443d5ed36e3e9d5841c299e1fcf78a62c
Sthefy Escobar <stefi.0592@gmail.com>
2023-05-31 18:13:51 -0500

    develop card component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header/Header";
+import CardContainer from "../../components/Cards/CardContainer";
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <CardContainer/>
     </div>
   );
 };
