64d44f68904449abc450fc3ddf9c6de40569b240
Oscar <64058694+OscarVarelaDev@users.noreply.github.com>
2023-06-02 23:54:57 -0600

    add btn back

--- src/pages/Detail/Detail.jsx
+++ src/pages/Detail/Detail.jsx
@@ -1,8 +1,16 @@
 import { Button, Card } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { useParams, Link, redirect, useNavigate } from "react-router-dom";
+
 import "./Detail.css";
 const Detail = () => {
   const [art, setArt] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+
+  //regresar a la pagina anterior
+
 
   const img_src = "https://media.admagazine.com/photos/618a6acbcc7069ed5077ca7f/master/w_1600%2Cc_limit/68704.jpg";
 
@@ -13,7 +21,10 @@ const Detail = () => {
         setArt(data);
       });
   };
+  const goBack = () => {
+    navigate('/');
 
+  }
 
   useEffect(() => {
     getArt();
@@ -23,62 +34,63 @@ const Detail = () => {
   return (
 
     <>
+      <Button
+        variant="outline-primary"
+        onClick={() => {
+          goBack();
+        }}
+        className="btn-regresar"
+      >Regresar</Button>
       <div className="container">
+
         <Card >
+
           <Card.Header>Titulo de la obra</Card.Header>
+
           <Card.Body>
 
             <div className="container-card">
               <div>
-                  <a href="https://media.admagazine.com/photos/618a6acbcc7069ed5077ca7f/master/w_1600%2Cc_limit/68704.jpg">
-                    <img src={img_src} alt="imagen galeria"
-                      className="img"
-                    />
-                  </a>
+                <Link to="https://media.admagazine.com/photos/618a6acbcc7069ed5077ca7f/master/w_1600%2Cc_limit/68704.jpg">
+                  <img src={img_src} alt="imagen galeria"
+                    className="img"
+                  />
+                </Link>
               </div>
               <div>
-                <Card.Title className="name-artist"
-                >Nombre Artista</Card.Title>
-                <Card.Text >
-                  <p>
-                    <b>Descripción</b>
-                  </p>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, voluptas? Voluptatibus quaerat similique aliquam ipsa sed mollitia error fuga recusandae! Autem tempora itaque quasi iure, omnis eveniet perspiciatis dolorum! Debitis.
-                  </p>
+                <Card.Title className="name-artist"><h2>Nombre Artista</h2>
+                </Card.Title>
+                <h3>
+                  <b>Descripción</b>
+                </h3>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, voluptas? Voluptatibus quaerat similique aliquam ipsa sed mollitia error fuga recusandae! Autem tempora itaque quasi iure, omnis eveniet perspiciatis dolorum! Debitis.
+                </p>
 
-                  <p>
-                    <b>Proceso de elavoración</b>
-                  </p>
+                <p> <b>Proceso de elavoración</b> </p>
+                <ul>
+                  <li>
+                    <p>Acriloco sobre tela</p>
+                  </li>
+                  <li>
 
-                  <p>
-                    <ul>
-                      <li>
-                        Acriloco sobre tela
+                    <p> Oleo sobre tela</p>
+                  </li>
 
-                      </li>
-                      <li>
-                        Oleo sobre tela
-                        </li>
-                      
-                        <li>
-                            Cera
-                        </li>
-                    </ul>
-                  </p>
-                  <p   >
-                    <b>Medidas</b>
-                  </p>
-                  <p>
-                    190 x 190 cm
-                  </p>
+                  <li>
+                    <p> Cera</p>
 
+                  </li>
+                </ul>
 
-                </Card.Text>
-
-                
+                <p  >
+                  <b>Medidas</b>
+                </p>
+                <p>
+                  190 x 190 cm
+                </p>
                 <Button variant="primary">Comprar </Button>
-             
+
               </div>
             </div>
           </Card.Body>
