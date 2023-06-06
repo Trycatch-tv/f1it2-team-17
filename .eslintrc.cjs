212015767335c47a6b9c714c36f52cdffbec6b49
oscar <oscarmunoz03@gmail.com>
2023-06-06 16:39:34 -0500

    Eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,20 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'standard'
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+  }
 }
